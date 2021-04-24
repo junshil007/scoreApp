2f9062a4e0723384c7cd164a161a63be41df92cb
ljsczf888999 <673839195@qq.com>
2021-04-24 14:13:22 +0800

    src

---
+++ main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import VConsole from "vconsole";
+
+new VConsole();
+
+Vue.config.productionTip = false
+Vue.use(Vant);
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  const type = to.meta.type
+  // 判断该路由是否需要登录权限
+  if (type === 'login') {
+    if (window.localStorage.getItem('login')) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()  // 确保一定要有next()被调用
+  }
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
